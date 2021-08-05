a46db27f38580ce6df634423805be25ba94e23e9
Andrew Dolgov <noreply@fakecake.org>
2021-08-05 08:04:11 +0300

    initial for ID range-based approach

--- README.md
+++ README.md
@@ -1,38 +1,29 @@
-# Automatically limit mount speed based on player riding skill and location
+# Automatically choose mounts based on player riding skill and location
 
-This module mimics mounts that "change their appearance based on rider skill and location" without creating multiple (five) spells for every mount and hardcoding those in Azeroth Core `spell_gen_mount()` (how Invincible is implemented).
+This module mimics mounts that "change their appearance based on rider skill and location" without hardcoding every mount in Azeroth Core `spell_gen_mount()` (how Invincible is implemented).
 
-Approach used for this module has some minor issues:
+Specific speed increase mount spells are selected, based on rider skill and location, as follows (baseSpell is the dummy mount spell, see `72286` as an example):
 
-- Speed buff text is not displayed in the UI;
-- Transitioning into `AREA_FLAG_NO_FLY_ZONE` (i.e. Dalaran city) is not handled correctly: the debuff doesn't unmount you for some reason, although it does stop you from flying;
+- `baseSpell + 1` = `60%`, ground
+- `baseSpell + 2` = `100%`, ground
+- `baseSpell + 3` = `150%`, flying
+- `baseSpell + 4` = `280%`, flying
+- `baseSpell + 5` = `310%`, flying
 
-Still, it's good enough for me, and could be useful as a proof of concept.
+All aforementioned spells should exist in `Spell.dbc`, otherwise spell script will fail validation and disable itself.
 
 ## Setup
 
 Checkout the module into `modules` server directory, and recompile the server.
 
-You will need to add 5 invisible service spells which apply actual speed increase and flying auras, those are hardcoded at IDs `240001` (60%) - `240005` (310%).
+For every new adaptive mount, you will need to add 5 specific speed increase spells and a basic dummy spell. Example spells are provided in `Spells.csv` (IDs `250010`-`250015`).
 
-Service spells are provided in included `Spells.csv`, import those into `Spells.dbc`.
-
-Then, add actual mount summoning spell.
-
-For the spell to work properly, it needs to have the following effects:
-
-1. `APPLY_AURA` -> `SPELL_AURA_MOUNTED` -> `NPC ID` is passed via Misc Value A. (this activates the mount)
-2. `APPLY_AURA` -> `SPELL_AURA_DUMMY`
-3. `SCRIPT_EFFECT`
-
-Module expects effects 2 and 3 (which normally do nothing) while effect 1 is needed to actually call in the mount. You can use base Invincible spell (`72286`) as an example.
-
-Then, assign spell script to your mount spell:
+Then, assign spell script to the dummy mount spell:
 
 ```sql
-insert into spell_script_names (spell_id, ScriptName) values (251003, 'spell_mount_autoselect');
+insert into spell_script_names (spell_id, ScriptName) values (250010, 'spell_mount_autoselect');
 ```
 
-Where `251003` is your custom mount spell. Example is, again, provided in `Spell.csv`.
+Where `250010` is your custom dummy mount spell. Example is, again, provided in `Spell.csv`.
 
 If everything worked correctly, you should be able to summon the mount everywhere, ground-only where appropriate, or flying if allowed.
--- src/MountAutoselect.cpp
+++ src/MountAutoselect.cpp
@@ -11,13 +11,6 @@
 enum MountModSpells
 {
     SPELL_COLD_WEATHER_FLYING           = 54197,
-
-    // these should be defined in Spells.dbc:
-    SPELL_MOUNT_SPEED_EFFECT_60         = 240001,
-    SPELL_MOUNT_SPEED_EFFECT_100        = 240002,
-    SPELL_MOUNT_SPEED_EFFECT_150        = 240003,
-    SPELL_MOUNT_SPEED_EFFECT_280        = 240004,
-    SPELL_MOUNT_SPEED_EFFECT_310        = 240005,
 };
 
 class MountAutoselect : public SpellScriptLoader
@@ -25,107 +18,108 @@ class MountAutoselect : public SpellScriptLoader
     public:
         MountAutoselect(): SpellScriptLoader("spell_mount_autoselect") { }
 
-        class MountAutoselect_AuraScript : public AuraScript
+        class MountAutoselect_SpellScript : public SpellScript
         {
-            PrepareAuraScript(MountAutoselect_AuraScript);
+            PrepareSpellScript(MountAutoselect_SpellScript);
 
-            bool Validate(SpellInfo const* /*spellInfo*/) override
-        {
-            if (!sSpellMgr->GetSpellInfo(SPELL_MOUNT_SPEED_EFFECT_60)
-                || !sSpellMgr->GetSpellInfo(SPELL_MOUNT_SPEED_EFFECT_100)
-                || !sSpellMgr->GetSpellInfo(SPELL_MOUNT_SPEED_EFFECT_150)
-                || !sSpellMgr->GetSpellInfo(SPELL_MOUNT_SPEED_EFFECT_280)
-                || !sSpellMgr->GetSpellInfo(SPELL_MOUNT_SPEED_EFFECT_310))
+            bool Validate(SpellInfo const* spellInfo) override
             {
-                return false;
+                int spellId = spellInfo->Id;
+
+                return sSpellMgr->GetSpellInfo(spellId) &&
+                    sSpellMgr->GetSpellInfo(spellId + 1) &&
+                    sSpellMgr->GetSpellInfo(spellId + 2) &&
+                    sSpellMgr->GetSpellInfo(spellId + 3) &&
+                    sSpellMgr->GetSpellInfo(spellId + 4) &&
+                    sSpellMgr->GetSpellInfo(spellId + 5);
             }
-            return true;
-        }
 
-            void OnApply(AuraEffect const* aurEff, AuraEffectHandleModes /*mode*/)
+            void ChooseMount(SpellEffIndex effIndex)
             {
-                Player* target = (Player*)GetTarget();
+                int spellId = GetSpellInfo()->Id;
 
-                /* mostly copy-pasted from spell_generic.cpp:4827 HandleMount() -fox */
+                //LOG_ERROR("scripts.spells", "Invoked for spell %d", spellId);
 
-                uint32 petNumber = target->GetTemporaryUnsummonedPetNumber();
-                target->SetTemporaryUnsummonedPetNumber(0);
+                int mount60 = spellId + 1;
+                int mount100 = spellId + 2;
+                int mount150 = spellId + 3;
+                int mount280 = spellId + 4;
+                int mount310 = spellId + 5;
 
-                // Triggered spell id dependent on riding skill and zone
-                bool canFly = false;
-                uint32 map = GetVirtualMapForMapAndZone(target->GetMapId(), target->GetZoneId());
-                if (map == 530 || (map == 571 && target->HasSpell(SPELL_COLD_WEATHER_FLYING)))
-                    canFly = true;
+                PreventHitDefaultEffect(effIndex);
 
-                AreaTableEntry const* area = sAreaTableStore.LookupEntry(target->GetAreaId());
-                // Xinef: add battlefield check
-                Battlefield* Bf = sBattlefieldMgr->GetBattlefieldToZoneId(target->GetZoneId());
-                if (!area || (canFly && ((area->flags & AREA_FLAG_NO_FLY_ZONE) || (Bf && !Bf->CanFlyIn()))))
-                    canFly = false;
-
-                uint32 mount = 0;
-                switch (target->GetBaseSkillValue(SKILL_RIDING))
+                if (Player* target = GetHitPlayer())
                 {
-                    case 0:
-                        mount = SPELL_MOUNT_SPEED_EFFECT_60;
-                        break;
-                    case 75:
-                        mount = SPELL_MOUNT_SPEED_EFFECT_100;
-                        break;
-                    case 150:
-                        mount = SPELL_MOUNT_SPEED_EFFECT_150;
-                        break;
-                    case 225:
-                        if (canFly)
-                            mount = SPELL_MOUNT_SPEED_EFFECT_150;
-                        else
-                            mount = SPELL_MOUNT_SPEED_EFFECT_100;
-                        break;
-                    case 300:
-                        if (canFly)
-                        {
-                            if (target->Has310Flyer(false))
-                                mount = SPELL_MOUNT_SPEED_EFFECT_310;
+                    uint32 petNumber = target->GetTemporaryUnsummonedPetNumber();
+                    target->SetTemporaryUnsummonedPetNumber(0);
+
+                    // Prevent stacking of mounts and client crashes upon dismounting
+                    target->RemoveAurasByType(SPELL_AURA_MOUNTED, ObjectGuid::Empty, GetHitAura());
+
+                    // Triggered spell id dependent on riding skill and zone
+                    bool canFly = false;
+                    uint32 map = GetVirtualMapForMapAndZone(target->GetMapId(), target->GetZoneId());
+                    if (map == 530 || (map == 571 && target->HasSpell(SPELL_COLD_WEATHER_FLYING)))
+                        canFly = true;
+
+                    AreaTableEntry const* area = sAreaTableStore.LookupEntry(target->GetAreaId());
+                    // Xinef: add battlefield check
+                    Battlefield* Bf = sBattlefieldMgr->GetBattlefieldToZoneId(target->GetZoneId());
+                    if (!area || (canFly && ((area->flags & AREA_FLAG_NO_FLY_ZONE) || (Bf && !Bf->CanFlyIn()))))
+                        canFly = false;
+
+                    uint32 mount = 0;
+                    switch (target->GetBaseSkillValue(SKILL_RIDING))
+                    {
+                        case 75:
+                            mount = mount60;
+                            break;
+                        case 150:
+                            mount = mount100;
+                            break;
+                        case 225:
+                            if (canFly)
+                                mount = mount150;
                             else
-                                mount = SPELL_MOUNT_SPEED_EFFECT_280;
-                        }
-                        else
-                            mount = SPELL_MOUNT_SPEED_EFFECT_100;
-                        break;
-                    default:
-                        break;
+                                mount = mount100;
+                            break;
+                        case 300:
+                            if (canFly)
+                            {
+                                if (target->Has310Flyer(false))
+                                    mount = mount310;
+                                else
+                                    mount = mount280;
+                            }
+                            else
+                                mount = mount100;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    //LOG_ERROR("scripts.spells", "Selecting spell %d based on skill %d canFly %d", mount, target->GetBaseSkillValue(SKILL_RIDING), canFly);
+
+                    if (mount)
+                    {
+                        PreventHitAura();
+                        target->CastSpell(target, mount, true);
+                    }
+
+                    if (petNumber)
+                        target->SetTemporaryUnsummonedPetNumber(petNumber);
                 }
-
-                //LOG_ERROR("scripts.spells", "Selecting spell %d based on skill %d canFly %d", mount, target->GetBaseSkillValue(SKILL_RIDING), canFly);
-
-                if (mount)
-                {
-                    target->CastSpell(target, mount, aurEff);
-                }
-
-                if (petNumber)
-                    target->SetTemporaryUnsummonedPetNumber(petNumber);
-            }
-
-            void OnRemove(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
-            {
-                Unit* target = GetTarget();
-
-                target->RemoveAurasByType(SPELL_AURA_MOUNTED, ObjectGuid::Empty);
-                target->RemoveAurasByType(SPELL_AURA_MOD_INCREASE_MOUNTED_SPEED, ObjectGuid::Empty);
-                target->RemoveAurasByType(SPELL_AURA_MOD_INCREASE_MOUNTED_FLIGHT_SPEED, ObjectGuid::Empty);
             }
 
             void Register() override
             {
-                OnEffectApply += AuraEffectApplyFn(MountAutoselect_AuraScript::OnApply, EFFECT_1, SPELL_AURA_DUMMY, AURA_EFFECT_HANDLE_REAL);
-                OnEffectRemove += AuraEffectRemoveFn(MountAutoselect_AuraScript::OnRemove, EFFECT_0, SPELL_AURA_MOUNTED, AURA_EFFECT_HANDLE_REAL);
+                OnEffectHitTarget += SpellEffectFn(MountAutoselect_SpellScript::ChooseMount, EFFECT_2, SPELL_EFFECT_SCRIPT_EFFECT);
             }
         };
 
-        AuraScript * GetAuraScript() const override
+        SpellScript * GetSpellScript() const override
         {
-            return new MountAutoselect_AuraScript();
+            return new MountAutoselect_SpellScript();
         }
 };
 
